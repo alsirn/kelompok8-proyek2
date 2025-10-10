b82843b60fb16ce888c4a4596ff265eb15eb0217
Amalia181205 <lutvianiamalia@gmail.com>
2025-10-10 08:04:23 +0700

    Perubahan

--- resources/views/bagianAwal/nav.blade.php
+++ resources/views/bagianAwal/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-custom">
   <div class="container">
-    <a class="navbar-brand" href="/">FotoBoth</a>
+    <a class="navbar-brand" href="/">FotoStudio</a>
     
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
