0b618b734433a825cce1ea03b2ec139ee43fa261
Alsirann <zahrangren78@gmail.com>
2025-10-10 22:30:57 +0700

    Update main.blade.php

--- resources/views/bagianAwal/main.blade.php
+++ resources/views/bagianAwal/main.blade.php
@@ -6,8 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <style>
-        /* Monochrome theme: black, white, gray */
+        body {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+        }
+        footer {
+            margin-top: auto;
+        }
     </style>
 </head>
 <body>
@@ -15,10 +23,8 @@
         {{-- @include('baginAwal.header') --}}
     </header>
     @include('bagianAwal.nav')
-    <div class="container mt-4">
-        @yield('content')
-    </div>
-    <footer class="bg-dark text-white text-center p-3 mt-4">
+    @yield('content')
+    <footer class="bg-dark text-white text-center p-3">
         @include('bagianAwal.footer')
         &copy; 2024 WebSaya.Com
     </footer>
